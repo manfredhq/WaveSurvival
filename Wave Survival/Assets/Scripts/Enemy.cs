b4843410b4c786b9b31ccfede7ae83acb49e16ab
Hugo <hugo.quitton@gmail.com>
2020-05-13 04:40:28 +0200

    init

---
+++ Wave Survival/Assets/Scripts/Enemy.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+
+    public GameObject target;
+    public int damage = 10;
+    public float speed = 10f;
+    public float range;
+    public NavMeshAgent agent;
+
+    public float attackBySecond = 1f;
+
+    private bool isAtacking = false;
+    private Base baseToTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseToTarget = target.GetComponent<Base>();
+        agent.SetDestination(target.transform.position);
+        agent.stoppingDistance = range;
+        agent.speed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*Debug.Log(agent.remainingDistance);
+        Debug.Log(agent.velocity);
+        Debug.Log(agent.hasPath);
+        Debug.Log(agent.isPathStale);*/
+        Debug.Log(agent.pathStatus);
+        if (!agent.pathPending)
+        {
+            NavMeshHit hit;
+            agent.Raycast(target.transform.position, out hit);
+            Debug.Log(hit.distance);
+            if (agent.remainingDistance < range && agent.destination != null && !isAtacking)
+            {
+                isAtacking = true;
+                StartCoroutine(Attack());
+            }
+            if (hit.hit && agent.pathStatus == NavMeshPathStatus.PathPartial)
+            {
+                if (hit.distance <= agent.stoppingDistance)
+                {
+
+                    Debug.Log(hit.GetType());
+                }
+            }
+
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        baseToTarget.TakeDamage(damage);
+        yield return new WaitForSeconds(1f / attackBySecond);
+    }
+}
---
+++ Wave Survival/Assets/Scripts/Wall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public int maxHp;
+    private int currentHp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHp = maxHp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHp -= damage;
+    }
+}
