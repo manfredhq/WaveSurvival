589876cadeab351a45a17f249fcf7232a9009696
Hugo <hugo.quitton@gmail.com>
2020-05-14 18:03:05 +0200

    Build perfect

--- Wave Survival/Assets/Scripts/BuildPrefab.cs
+++ Wave Survival/Assets/Scripts/BuildPrefab.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,18 +8,23 @@ public class BuildPrefab : MonoBehaviour
 {
     public List<GameObject> objects = new List<GameObject>();
     public List<Collider> colliders = new List<Collider>();
+    public Material canBuild;
+    public Material cantBuild;
     public GameObject player;
     public float offset = 2f;
     public bool isBuild = false;
     public bool isBuildingMode = true;
     public bool isBuildable = true;
 
+    private List<Material> oldMat = new List<Material>();
     private void Start()
     {
         if (isBuildingMode)
         {
             foreach (GameObject obj in objects)
             {
+                oldMat.Add(obj.GetComponent<Renderer>().material);
+                obj.GetComponent<Renderer>().material = canBuild;
                 obj.GetComponent<NavMeshObstacle>().enabled = false;
             }
             transform.position = GameManager.instance.playerGroundPosition + player.transform.localRotation * Vector3.forward * offset;
@@ -26,16 +32,22 @@ public class BuildPrefab : MonoBehaviour
     }
     private void Update()
     {
-        if (isBuildingMode)
+        if (!isBuild)
         {
-            transform.rotation = player.transform.rotation;
-            transform.position = GameManager.instance.playerGroundPosition + player.transform.localRotation * Vector3.forward * offset;
-            if (Input.GetKeyDown(KeyCode.Return))
+            if (isBuildingMode && isBuildable)
             {
-                Build();
+
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    Build();
+                }
+            }
+            if (isBuildingMode)
+            {
+                transform.rotation = player.transform.rotation;
+                transform.position = GameManager.instance.playerGroundPosition + player.transform.localRotation * Vector3.forward * offset;
             }
         }
-
     }
 
     public void Build()
@@ -43,7 +55,34 @@ public class BuildPrefab : MonoBehaviour
         isBuildingMode = false;
         foreach (GameObject obj in objects)
         {
+            obj.GetComponent<Renderer>().material = oldMat[0];
             obj.GetComponent<NavMeshObstacle>().enabled = true;
+            oldMat.RemoveAt(0);
+        }
+        isBuild = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!isBuild)
+        {
+            isBuildable = false;
+            foreach (GameObject obj in objects)
+            {
+                obj.GetComponent<Renderer>().material = cantBuild;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!isBuild)
+        {
+            isBuildable = true;
+            foreach (GameObject obj in objects)
+            {
+                obj.GetComponent<Renderer>().material = canBuild;
+            }
         }
     }
 }
