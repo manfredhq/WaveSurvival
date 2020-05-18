e448c3228831a3ab55e8352f54c9f47c7b98b82c
Hugo <hugo.quitton@gmail.com>
2020-05-19 01:21:32 +0200

    Update MobSpawnerManager.cs

--- Wave Survival/Assets/Scripts/MobSpawnerManager.cs
+++ Wave Survival/Assets/Scripts/MobSpawnerManager.cs
@@ -6,6 +6,9 @@ public class MobSpawnerManager : MonoBehaviour
 {
     public List<GameObject> mobPrefabs = new List<GameObject>();
     public List<SpawnPoint> spawnPoints = new List<SpawnPoint>();
+    public List<Wave> Waves = new List<Wave>();
+    private List<SpawnPoint> currentWaveSpawnpoints = new List<SpawnPoint>();
+    private int waveIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,9 +26,32 @@ public class MobSpawnerManager : MonoBehaviour
 
     public void SpawnMobs()
     {
-        foreach (var spawnPoint in spawnPoints)
+        List<int> randomNumber = new List<int>();
+        currentWaveSpawnpoints.Clear();
+        for (int i = 0; i < Waves[waveIndex].numberSpawn; i++)
+        {
+            int temp = Random.Range(0, spawnPoints.Count);
+            if (randomNumber.Contains(temp))
+            {
+                i--;
+            }
+            else
+            {
+                randomNumber.Add(temp);
+                currentWaveSpawnpoints.Add(spawnPoints[temp]);
+            }
+        }
+        foreach (var spawnPoint in currentWaveSpawnpoints)
         {
             StartCoroutine(spawnPoint.SpawnEnemies(3, mobPrefabs[Random.Range(0, mobPrefabs.Count)], 2));
         }
+        waveIndex++;
     }
+}
+
+[System.Serializable]
+
+public class Wave
+{
+    public int numberSpawn;
 }
